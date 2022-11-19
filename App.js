d9f3cf6b8e2aaec67f86281f60c1e2a5bc893344
Uslipe <107627690+Uslipe@users.noreply.github.com>
2022-11-18 22:18:17 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image source={logo} style={{width:256, height:256}}/>
+      <Text>Felipe Pereira Reis</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
